6bebe277c998e83fb3738d30086cb6e64164a054
kelvinunix47 <kelvinunix47@gmail.com>
2025-11-10 00:48:44 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "blinder~cMQ2iJAS#gU9FuF6MeD2WXpwJF5gon4ODhjC0O5OI4b9fDnx_yFo",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~cMQ2iJAS#blinder~xdwEmDAa#U6vu7bfY6-IcDsjEZkEyXbQd_fYsTv3HBFR2Ic6bftY",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
